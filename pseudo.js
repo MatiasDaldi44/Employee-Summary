11dab18ee5d20da557a1af0be3441e77e2c113b2
MD24 <matiasdaldi@gmail.com>
2020-06-03 21:33:44 -0400

    commit

---
+++ pseudo.js
@@ -0,0 +1,33 @@
+// npm install
+
+// GETTING USER INPUT
+// 1. Build out how our inquirer is going to work
+    // ask questions to build a manager
+        // what is your manager's name, id, email, office number
+        // once you get the answers, this will probably finish one inquirer and you will build the manager
+
+// 2. Ask whether you want to add an engineer, intern, or nothing else
+    // ask questions to build an engineer
+        // what is your engineers' name, id, email, github username
+    // ask questions to build an intern
+        // what is your intern's name, id, email, school
+
+// 3. Figure our how to repeat until the 'no more team members' option is chosen
+
+// CLASSES
+// 1. Think about what properties each class has
+// 2. Write the properties and methods that each class expects
+
+
+
+// .then(function (answers) {
+
+// })
+// type
+// name
+// message
+// validate: (answer) => 
+// if (answer !== )
+
+// type
+// name
